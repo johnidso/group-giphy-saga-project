1ff59f110e07201975d0683414c745e72b6e5abe
Hann1234 <christopherjhannigan@gmail.com>
2021-07-22 09:58:45 -0500

    added saga notation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// bringing in redux-saga into our project
+import createSagaMiddleware from 'redux-saga';
+import { put, takeEvery, call } from 'redux-saga/effects';
+
+import logger from 'redux-logger';
+import axios from 'axios';
+
+// Reducers:
+
+// Saga:
+
+function* watcherSaga() {
+
+} 
+
+function* fetchGifs() {
+    
+}
+
+function* postGifs() {
+
+}
+
+function* putCategory() {
+
+}
+
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+// Store:
+
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(watcherSaga);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={storeInstance}><App/></Provider>, document.getElementById('root'));
+
+
+
