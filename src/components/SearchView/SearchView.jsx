e2abf8f0c8557c19afb1f0d8dac0b766ae7ff7d9
Tim Dugan <tadugan@me.com>
2021-07-22 13:47:52 -0500

    Added Router to App.jsx

--- src/components/SearchView/SearchView.jsx
+++ src/components/SearchView/SearchView.jsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
 function SearchView() {
+    
+    const dispatch = useDispatch();
+    const [ searchText, setSearchText ] = useState('');
+    
+    const handleChange = (event) => {
+        setSearchText(event.target.value);        
+    }
+
+    const handleSubmit = () => {
+        event.preventDefault();
+        // Validate Input
+        if (searchText === '') {
+            alert('Please enter a search input.') // TODO: replace this with an update to the DOM 
+            return;
+        }
+
+        console.log('This is when we dispatch to saga'); // test
+
+        dispatch({ type: 'SEARCH_GIPHY', payload: searchText});
+        setSearchText('');
+    }
+
     return (
-        <p>SearchView content</p>
+        <div>
+            <p>SearchView content</p>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    name="searchField"
+                    value={searchText}
+                    onChange={handleChange} 
+                />
+                <button type="submit">
+                    Search
+                </button>
+            </form>
+            <p>display searched gif here:</p>
+            <img src="https://im.rediff.com/news/2020/sep/15funny1.jpg" />
+        </div>
     );
 }
 
